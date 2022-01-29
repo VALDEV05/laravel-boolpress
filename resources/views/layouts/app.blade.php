8831d139a5ffb272d1d2e2e68ee2f2adfb609be7
Valerio Corda <corda.valerio05@GMAIL.COM>
2022-01-29 14:49:42 +0100

    Adjusted layout and view home visitors

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,13 +26,6 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
@@ -40,14 +33,14 @@
                             <a class="nav-link" href="{{ route('guest.home') }}"> <i class="fa fa-home fa-lg fa-fw"></i></a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">About</a>
+                            <a class="nav-link" href="#"><i class="far fa-address-card  fa-lg fa-fw"></i></a>
                             
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Contacts</a>
+                            <a class="nav-link" href="#"><i class="fas fa-phone-alt  fa-lg fa-fw "></i></a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('posts.index') }}">Blog</a>
+                            <a class="nav-link" href="{{ route('posts.index') }}"><i class="fas fa-newspaper fa-lg fa-fw"></i></a>
                         </li>
                     </ul>
 
